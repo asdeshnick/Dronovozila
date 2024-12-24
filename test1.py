202768b2629185a5527366eec4a25812d1d16c64
Andrey <andrejmarcenko334@gmail.com>
2024-12-24 20:59:54 +0700

    1.8v

--- test1.py
+++ test1.py
@@ -4,6 +4,8 @@ import time
 from main import load_model
 from ultralytics import YOLO
 from tqdm import tqdm
+import torch
+
 
 # Функция обработки кликов мыши
 selected_id = None
@@ -12,7 +14,7 @@ def select_bbox(event, x, y, flags, param):
     global selected_id,bboxes
     
     if event == cv2.EVENT_LBUTTONDOWN:
-    # print(event, x, y)
+        print(event, x, y)
     # if event == 1:
         
         for bbox in bboxes:
@@ -35,7 +37,7 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 
 
 frame_number = 0
-step_detection = 3
+step_detection = 9
 class_name = 0
 
 total_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -44,10 +46,13 @@ st_time = time.time()
 # for frame_number in tqdm(range(total_frame_count), desc='Обработка кадров для ящиков'):
 while True:
     success, frame = cap.read()
+    if frame_number == 0:
+        frame_x_c = frame.shape[1] // 2
+        frame_y_c= frame.shape[0] // 2
     if not success or frame_number >= total_frame_count:
         break
-    if frame_number % step_detection == 0:
 
+    if frame_number % step_detection == 0:
         results = model.track(frame, classes = [0], persist=True, verbose=False, imgsz=640)
         bboxes = []
         for result in results:
@@ -55,25 +60,45 @@ while True:
                 x1,y1,x2,y2 = map(int, box.xyxy[0])
                 track_id = int(box.id)
                 bboxes.append([x1,y1,x2,y2, track_id])
-        if selected_id is not None:
-            for bbox in bboxes:
-                if bbox[4] == selected_id:
-                    cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
-                    cv2.putText(frame, f'ID: {selected_id}', (bbox[0], bbox[1] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-                    img = frame
-                    break
-        else:
-            img = results[0].plot() 
+    id_check = 0
+    if selected_id is not None:
+        for bbox in bboxes:
+            if bbox[4] == selected_id:
+                id_check = 1
+                x_center = (bbox[0] + bbox[2]) // 2
+                y_center = (bbox[1] + bbox[3]) // 2
+                print("по x на", x_center - frame_x_c)
+                print("по y на", y_center - frame_y_c)
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
+                cv2.putText(frame, f'ID: {selected_id}', (bbox[0], bbox[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                # img = frame
+                 
+        if id_check == 0:
+            selected_id = None
+
+    elif selected_id is None:
+        for bbox in bboxes:
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
+            cv2.putText(frame, f'ID: {bbox[4]}', (bbox[0], bbox[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            # img = frame
+
+
+        # img = results[0].plot() 
         # img = results.plot()
-        
-        cv2.imshow("Tracking", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-            # key = cv2.waitKey(1)
-            # if key == 27:  # esc
-            #     print("esc pressed")
-            #     break
+    # else:
+    #     img = frame
+    cv2.imshow("Tracking", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    elif key == ord('r'):
+        selected_id = None
+        # key = cv2.waitKey(1)
+        # if key == 27:  # esc
+        #     print("esc pressed")
+        #     break
     frame_number += 1
 
 print(time.time() - st_time)
